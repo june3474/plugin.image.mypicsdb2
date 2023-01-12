3e35af0efe93334f83059e54ee3bac94f0d3ac70
dks <june3474@gmail.com>
2023-01-12 09:41:52 +0900

    Add context menu item

--- contextitem.py
+++ contextitem.py
@@ -4,10 +4,15 @@ import sys
 import xbmc
 
 def main():
-    arg = sys.argv[1]
-    if arg == 'folder':
+    if sys.argv[1] == 'folder':
         folder = xbmc.getInfoLabel('ListItem.folderPath')
         xbmc.executebuiltin('RecursiveSlideShow(\"%s\")' % folder)
+        
+    if sys.argv[1] == 'picture':
+        folder = xbmc.getInfoLabel('ListItem.folderPath')
+        filepath = xbmc.getInfoLabel('ListItem.FileNameAndPath')
+        xbmc.executebuiltin('SlideShow(\"%s\", recursive, \
+                             beginslide=\"%s\")' % (folder, filepath))        
 
 if __name__ == '__main__':
     main()
